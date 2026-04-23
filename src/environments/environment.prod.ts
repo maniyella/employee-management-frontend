30dbec5e9b7604e557d520a3abcc94b898b0f082
maneeshayella <maneeshayella@gmail.com>
2026-04-23 16:51:00 +0530

    changed url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://employee-management-backend-production-6eb1.up.railway.app/'
+  apiUrl: 'https://employee-management-backend-production-6eb1.up.railway.app'
 };
