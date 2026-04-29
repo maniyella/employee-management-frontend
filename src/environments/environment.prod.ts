101e62380fbcae10e465d55af5a902b5c18deec5
maneeshayella <159248391+maniyella@users.noreply.github.com>
2026-04-29 15:45:47 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://employee-management-backend-production-b12a.up.railway.app/'
+  apiUrl: 'https://employee-management-backend-production-b12a.up.railway.app'
 }
